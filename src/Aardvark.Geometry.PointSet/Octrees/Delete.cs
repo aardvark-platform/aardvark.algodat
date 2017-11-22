da2b0d7fae47f3ce40064de90f34d88d28e8a502
Stefan Maierhofer <sm@stefanmaierhofer.com>
2017-11-22 13:21:58 +0100

    [PointSet] immutable delete (wip)

---
+++ src/Aardvark.Geometry.PointSet/Octrees/Delete.cs
@@ -0,0 +1,52 @@
+﻿/*
+    Copyright (C) 2017. Aardvark Platform Team. http://github.com/aardvark-platform.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aardvark.Base;
+
+namespace Aardvark.Geometry.Points
+{
+    /// <summary>
+    /// </summary>
+    public static class DeleteExtensions
+    {
+        /// <summary>
+        /// Returns new pointset with all points deleted which are inside.
+        /// </summary>
+        public static PointSet Delete(this PointSet node,
+            Func<PointSetNode, bool> isNodeFullyInside,
+            Func<PointSetNode, bool> isNodeFullyOutside,
+            Func<V3d, bool> isPositionInside
+            )
+        {
+            var root = Delete(node.Root.Value, isNodeFullyInside, isNodeFullyOutside, isPositionInside);
+            var newId = Guid.NewGuid().ToString();
+            var result = new PointSet(node.Storage, newId, root.Id, node.SplitLimit);
+            return result;
+        }
+
+        /// <summary>
+        /// </summary>
+        public static PointSetNode Delete(this PointSetNode node,
+            Func<PointSetNode, bool> isNodeFullyInside,
+            Func<PointSetNode, bool> isNodeFullyOutside,
+            Func<V3d, bool> isPositionInside
+            )
+
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
