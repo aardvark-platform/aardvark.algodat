cd7aaf57c24a2e4b9d3388fc06644cec3e73883b
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-10-29 09:56:09 +0100

    sync

---
+++ src/Aardvark.Geometry.PointSet/Views/IPointCloudNode.cs
@@ -0,0 +1,63 @@
+﻿/*
+    Copyright (C) 2006-2018. Aardvark Platform Team. http://github.com/aardvark-platform.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Aardvark.Base;
+using System;
+using System.Collections.Generic;
+
+namespace Aardvark.Geometry.Points
+{
+    /// <summary>
+    /// An immutable point cloud octree node.
+    /// </summary>
+    public interface IPointCloudNode
+    {
+        /// <summary>
+        /// This node's index/bounds.
+        /// </summary>
+        Cell Cell { get; }
+
+        /// <summary>
+        /// Center of this node's cell.
+        /// </summary>
+        V3d Center { get; }
+
+        /// <summary>
+        /// Number of points in this tree (sum of leaves).
+        /// </summary>
+        long PointCountTree { get; }
+        
+        /// <summary>
+        /// Subnodes (8), or null if leaf.
+        /// Entries are null if there is no subnode.
+        /// There is at least 1 non-null entry.
+        /// </summary>
+        PersistentRef<IPointCloudNode>[] Subnodes { get; }
+
+        /// <summary>
+        /// </summary>
+        bool HasProperty(string name);
+
+        /// <summary>
+        /// </summary>
+        bool TryGetProperty<T>(string name, out PersistentRef<T> result) where T : class;
+
+        /// <summary>
+        /// </summary>
+        bool HasPositions { get; }
+
+        /// <summary>
+        /// </summary>
+        bool HasLodPositions { get; }
+    }
+}
---
+++ src/Aardvark.Geometry.PointSet/Views/IPointCloudNodeExtensions.cs
@@ -0,0 +1,50 @@
+﻿/*
+    Copyright (C) 2006-2018. Aardvark Platform Team. http://github.com/aardvark-platform.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Aardvark.Base;
+using System;
+using System.Collections.Generic;
+
+namespace Aardvark.Geometry.Points
+{
+    /// <summary>
+    /// </summary>
+    public static class IPointCloudNodeExtensions
+    {
+        /// <summary></summary>
+        public static bool IsLeaf(this IPointCloudNode self) => self.Subnodes == null;
+
+        /// <summary></summary>
+        public static bool IsNotLeaf(this IPointCloudNode self) => self.Subnodes != null;
+
+        /// <summary>
+        /// Counts ALL nodes of this tree by traversing over all persistent refs.
+        /// </summary>
+        public static long CountNodes(this IPointCloudNode self)
+        {
+            if (self == null) return 0;
+
+            var subnodes = self.Subnodes;
+            if (subnodes == null) return 1;
+            
+            var count = 1L;
+            for (var i = 0; i < 8; i++)
+            {
+                var n = subnodes[i];
+                if (n == null) continue;
+                count += n.Value.CountNodes();
+            }
+            return count;
+        }
+    }
+}
