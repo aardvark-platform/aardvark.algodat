22d9c8e054822d7089ff10cccf24b44a24fcf3eb
aszabo <aszabo@vrvis.at>
2019-03-28 18:27:32 +0100

    cleanup

--- src/Apps/Viewer/Program.fs
+++ src/Apps/Viewer/Program.fs
@@ -13,25 +13,21 @@ let main args =
 
     let args = Args.parse args
 
-    import @"D:\pts\Laserscan-MS60_Beiglboeck-2015.pts" @"D:\store" "test233" args
-    view @"D:\store" "test233" args
+    match args.command with
 
+    | Some (Info filename) -> info filename args
 
-    //match args.command with
-
-    //| Some (Info filename) -> info filename args
-
-    //| Some (Import (filename, store, key)) -> import filename store key args
-        
-    //| Some (View (store, key)) ->
-    //    view store key args
-        
-    //| Some Gui ->
-    //    failwith "not implemented"
-        
-    //| Some (Download (baseurl, targetdir)) -> download baseurl targetdir args
-        
-    //| None ->
-    //    printUsage()
+    | Some (Import (filename, store, key)) -> import filename store key args
+      
+    | Some (View (store, key)) ->
+        view store key args
+      
+    | Some Gui ->
+        failwith "not implemented"
+      
+    | Some (Download (baseurl, targetdir)) -> download baseurl targetdir args
+      
+    | None ->
+        printUsage()
 
     0
