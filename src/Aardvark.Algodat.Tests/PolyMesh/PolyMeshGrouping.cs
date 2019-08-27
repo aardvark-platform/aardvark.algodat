59ea38b4ab98a9656b14591b8c5531ce26761711
Christian Luksch <luksch@vrvis.at>
2019-08-27 15:11:11 +0200

    [Test] added PolyMeshGrouping test from AARDVARK-256;

---
+++ src/Aardvark.Algodat.Tests/PolyMesh/PolyMeshGrouping.cs
@@ -0,0 +1,42 @@
+﻿using Aardvark.Base;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aardvark.Geometry.Tests
+{
+    [TestFixture]
+    public class PolyMeshGrouping
+    {
+        [Test]
+        public void TestPolyMeshGrouping()
+        {
+            var mesh = PolyMeshPrimitives.Cone(12, 1, 0.5, C4b.Red);
+
+            // NOTE: mesh has FaceVertexNormals (non-indexed)
+            ValidateNormals(mesh);
+
+            var grouping = mesh.IntoArray().Group();
+
+            var meshWithBrokenFaceVertexAttributes = grouping.Mesh; // normal array broken
+
+            // NOTE: grouped mesh will also have non-indexed FaceVertexNormals, but half of the data is "zero"
+            ValidateNormals(meshWithBrokenFaceVertexAttributes);
+        }
+
+        static void ValidateNormals(PolyMesh mesh)
+        {
+            mesh.Faces.ForEach(f =>
+            {
+                var fn = f.Polygon3d.ComputeNormal();
+                for (int i = 0; i < f.VertexCount; i++)
+                {
+                    // check if face vertex normal points in similar direction
+                    var fvn = f.GetVertexAttribute<V3d>(PolyMesh.Property.Normals, 0);
+                    Assert.True(fvn.Dot(fn) > 0);
+                }
+            });
+        }
+    }
+}
