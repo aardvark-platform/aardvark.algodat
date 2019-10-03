ce2e248a08f6493dc75e6f1bec540aae11735f19
aszabo <aszabo@vrvis.at>
2019-10-03 15:23:53 +0200

    upgraded build script (msbuild visual studio 2019)

--- build.cmd
+++ build.cmd
@@ -2,28 +2,21 @@
 SETLOCAL
 PUSHD %~dp0
 
-ECHO bootstrapping package manager ...
-.paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-SET FSI_PATH=packages\build\FAKE\tools\Fake.exe
-
-IF exist boot.fsx ( 
-    "%FSHARPINSTALLDIR%fsi.exe" "boot.fsx" 
-    del "boot.fsx"
-	.paket\paket.exe install
+IF NOT exist .paket\paket.exe (
+	dotnet tool install Paket --tool-path .paket
 )
 
 if NOT exist paket.lock (
-	echo No paket.lock found, running paket install.
-	.paket\paket.exe install
+    echo No paket.lock found, running paket install.
+    .paket\paket.exe install
 )
 
-.paket\paket.exe restore --group Build
+.paket\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-"%FSI_PATH%" "build.fsx" Dummy --fsiargs build.fsx %* 
+dotnet packages\build\fake-cli\tools\netcoreapp2.1\any\fake-cli.dll build %* 
+
+
+
--- build.fsx
+++ build.fsx
@@ -1,11 +1,20 @@
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
 #load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
 
-open Fake
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
 
+do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
 DefaultSetup.install ["src/Aardvark.Algodat.sln"]
+
+
+#if DEBUG
+do System.Diagnostics.Debugger.Launch() |> ignore
+#endif
+
 
 entry()
