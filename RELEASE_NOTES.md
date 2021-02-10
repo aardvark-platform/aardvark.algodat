0f8dfa546f176836462f6910c80012ba442d9a48
Harald Steinlechner <haraldsteinlechner@gmail.com>
2021-02-10 19:17:20 +0100

    upated to aardvark.base 5.1/FSharp.Data.Adaptive 1.1 track

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+### 5.1.1
+- updated to FSharp.Data.Adaptive 1.1 and base 5.1 track
+
--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ SETLOCAL
 PUSHD %~dp0
 
 IF NOT exist .paket\paket.exe (
-	dotnet tool install Paket --tool-path .paket --version 6.0.0-alpha050
+	dotnet tool install Paket --tool-path .paket --version 5.257.0
 )
 
 if NOT exist paket.lock (
