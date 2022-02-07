0b79f9b5fe631b65f7b79662e6d9904f40e3f701
aszabo314 <aszabo@vrvis.at>
2022-02-07 11:14:02 +0100

    Aardvark.Build

--- publish.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL
-PUSHD %~dp0
-
-dotnet publish src\Apps\Viewer\Viewer.fsproj -c Release -o "%~dp0\bin\Viewer\win10-x64" -r win10-x64 --self-contained
-dotnet publish src\Apps\ImportTest\ImportTest.csproj -c Release -o "%~dp0\bin\ImportTest\win-x64" -r win10-x64 --self-contained
-dotnet publish src\Apps\points\points.fsproj -c Release -o "%~dp0\bin\points\win-x64" -r win10-x64 --self-contained
-
